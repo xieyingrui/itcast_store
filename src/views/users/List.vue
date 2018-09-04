5e27c49a86ecb2003f1f3c9518b2489ecacf99a2
xieyingrui <270823983@qq.com>
2018-09-04 16:46:21 +0800

    搜索框

--- src/views/users/List.vue
+++ src/views/users/List.vue
@@ -5,6 +5,13 @@
             <el-breadcrumb-item><a href="/">用户管理</a></el-breadcrumb-item>
             <el-breadcrumb-item>用户列表</el-breadcrumb-item>
         </el-breadcrumb>
+        <el-row class="row">
+            <el-col :span="24">
+                <el-input style="width:300px" placeholder="请输入内容" v-model="input5" class="input-with-select">
+                    <el-button slot="append" icon="el-icon-search"></el-button>
+                </el-input>
+            </el-col>
+        </el-row>
     </el-card>
 </template>
 
@@ -17,5 +24,8 @@ export default {
 <style>
     .card{
         height: 100%;
+    }
+    .row {
+        margin:20px 0;
     }
 </style>
