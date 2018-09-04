2df9e2a8afe652a0fc4f8e011269d8bb304de790
xieyingrui <270823983@qq.com>
2018-09-04 17:04:33 +0800

    用户表格部分

--- src/views/users/List.vue
+++ src/views/users/List.vue
@@ -12,12 +12,56 @@
                 </el-input>
             </el-col>
         </el-row>
+        <el-table
+            border
+            stripe
+            :data="tableData"
+            style="width: 100%">
+            <el-table-column
+                type="index"
+                width="50">
+            </el-table-column>
+            <el-table-column
+                prop="date"
+                label="日期"
+                width="180">
+            </el-table-column>
+            <el-table-column
+                prop="name"
+                label="姓名"
+                width="180">
+            </el-table-column>
+            <el-table-column
+                prop="address"
+                label="地址">
+            </el-table-column>
+        </el-table>
     </el-card>
 </template>
 
 <script>
 export default {
-
+ data() {
+    return {
+        tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+        }]
+    }
+}
 };
 </script>
 
